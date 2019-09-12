a658396fa6eec0899887632af14d318f8bbaac79
Adesola Oni-Shogbonyo <s.oni-shogbonyo@hotmail.co.uk>
2019-09-12 12:22:36 +0100

    Ready for deployment for presentation

--- project/settings.py
+++ project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -137,3 +137,5 @@ REST_FRAMEWORK = {
 }
 
 AUTH_USER_MODEL = 'jwt_auth.User'
+
+django_heroku.settings(locals())
